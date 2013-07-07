3c7654e4d47215c95c39bfda4b2451dc5d0a45ad
rgfrazier <rgfbiz@gmail.com>
2013-07-07 17:04:13 +0000

    Added new web.js

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 //var greeting = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-    fs.readFileSync(index.html).toString(greeting)
+    var greeting = fs.readFileSync(index.html).toString()
 //  fs.readFileSync(index.html) | buf.toString(greeting)
-  response.send(greeting);
+  response.send('greeting');
 });
 
 var port = process.env.PORT || 5000;
